be8f589d911bd9287e2af3176a0c026a12d5a756
Dan-J-D <52302961+Dan-J-D@users.noreply.github.com>
2021-03-02 11:52:15 -0500

    fixed missing bin folder on compilation

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
+mkdir ./bin
 g++ -D_WIN32_WINNT=0x0600 -I ./include -march=native -O3 ./test/test.cpp ./src/rand/rand.cpp ./src/sha3/sha3.cpp ./src/chacha20-poly1305/chacha20-poly1305.cpp ./src/chacha20-poly1305/chacha20-poly1305/rfc8439.cpp ./src/firesaber/firesaber.cpp ./src/firesaber/firesaber/poly_mul.cpp ./src/firesaber/firesaber/pack_unpack.cpp ./src/firesaber/firesaber/cbd.cpp ./src/firesaber/firesaber/poly.cpp ./src/firesaber/firesaber/SABER_indcpa.cpp ./src/firesaber/firesaber/verify.cpp ./src/firesaber/firesaber/kem.cpp -o ./bin/build.exe
